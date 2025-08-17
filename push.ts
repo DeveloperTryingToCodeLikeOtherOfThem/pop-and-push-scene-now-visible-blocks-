921cc02c2c07e81efc06c642d5cbe02db66a2384
TryingToCodeLikeThem <Aeagan0507@gmail.com>
2025-08-17 10:12:26 -0700

    Initial files for MakeCode project

---
+++ push.ts
@@ -0,0 +1,6 @@
+namespace game {
+    //% block="push scene" blockNamespace=game
+    export namespace pushScene { }
+      //% block="push scene" blockNamespace=game
+    export namespace popScene {}
+}
